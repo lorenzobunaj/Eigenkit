60cd1297b1774b5014feac249c45f011fd405ec7
lorenzobunaj <lorenzobunaj@gmail.com>
2023-04-24 00:14:25 +0200

    fixed bugs

--- Eigenkit/Quaternions.h
+++ Eigenkit/Quaternions.h
@@ -1,6 +1,8 @@
-#ifndef QUATERNIONS_H_INCLUDED
-#define QUATERNIONS_H_INCLUDED
-
-
-
-#endif // QUATERNIONS_H_INCLUDED
+#include "src/Quaternions/Quaternion/Quaternion.h"
+#include "src/Quaternions/Quaternion/Constructors.h"
+#include "src/Quaternions/Quaternion/QFunctions.h"
+#include "src/Quaternions/Algebra/Arithmetic.h"
+#include "src/Quaternions/Notations/Matrix.h"
+#include "src/Quaternions/Notations/Vector.h"
+#include "src/Quaternions/Transformations/Reflections.h"
+#include "src/Quaternions/Transformations/Rotations.h"
