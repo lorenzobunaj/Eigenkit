b73ec8f4825c0f2c635b3efce9fdcb4e7277bcc6
lorenzobunaj <lorenzobunaj@gmail.com>
2023-06-09 00:21:50 +0200

    implemented qr givens factorization

--- Eigenkit/src/Dense/Decompositions/QR/GivensQR.h
+++ Eigenkit/src/Dense/Decompositions/QR/GivensQR.h
@@ -1,1 +1,45 @@
+namespace ek
+{
+    template <typename T>
+    class GivensQR
+    {
+    private:
+        std::vector<Matrix<T>> operators;
 
+        std::vector<Matrix<T>> givensize(Matrix<T> A)
+        {
+            Identity<T> id(A.rows());
+            Matrix<T> Qt = id;
+            Matrix<T> R = A;
+
+            for (size_t j=0; j<A.cols(); j++) {
+                for (size_t i=j+1; i<A.rows(); i++) {
+                    if (A(i,j) == 0) {continue;}
+
+                    T r = std::sqrt(std::pow(R(j,j),2)+std::pow(R(i,j),2));
+                    T c = R(j,j)/r;
+                    T s = -R(i,j)/r;
+
+                    Givens<T> G(A.rows(),j,i,c,s);
+
+                    Qt = G*Qt;
+                    R = G*R;
+                }
+            }
+
+            return {Qt.t(), R};
+        }
+    public:
+        Matrix<T> A;
+        Matrix<T> Q;
+        Matrix<T> R;
+
+        GivensQR(Matrix<T> mtx)
+        {
+            operators = givensize(mtx);
+            A = mtx;
+            Q = operators[0];
+            R = operators[1];
+        };
+    };
+}
--- Eigenkit/src/Dense/Decompositions/Transformations/Givens.h
+++ Eigenkit/src/Dense/Decompositions/Transformations/Givens.h
@@ -4,25 +4,26 @@ namespace ek
     class Givens : public Transformation<T>
     {
     private:
-        std::vector<std::vector<T>> givensize(size_t s, size_t ir, size_t ic, T a)
+        std::vector<std::vector<T>> givensize(size_t d, size_t p1, size_t p2, T c, T s)
         {
-            std::vector<std::vector<T>> arr;
+            Identity<T> id(d);
+            std::vector<std::vector<T>> arr = id.mtx();
 
-            arr.resize(s);
+            /*arr.resize(r);
             size_t i=0;
             for (auto r = arr.begin(); r != arr.end(); r++) {
-                (*r).resize(s);
+                (*r).resize(c);
                 (*r)[i++] = 1;
-            }
-            arr[ir][ir] = std::cos(a);
-            arr[ic][ic] = std::cos(a);
-            arr[ir][ic] = -std::sin(a);
-            arr[ic][ir] = std::sin(a);
+            }*/
+            arr[p1][p1] = c;
+            arr[p2][p2] = c;
+            arr[p1][p2] = -s;
+            arr[p2][p1] = s;
 
             return arr;
         }
     public:
         // special constructor
-        Givens(size_t s, size_t ir, size_t ic, T a) : Transformation<T>(givensize(s,ir,ic,a)){};
+        Givens(size_t d, size_t p1, size_t p2, T c, T s) : Transformation<T>(givensize(d,p1,p2,c,s)){};
     };
 }
